48f7fd6e3e659db8d6a8e8c84832a29d2d30664c
Shogo Fukushima <59794072+fksms@users.noreply.github.com>
2024-07-19 12:15:23 +0900

    Added vite-plugin-sitemap

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,18 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
+import sitemap from "vite-plugin-sitemap";
+
+const hostname = "https://fksms.github.io/";
+const route = "/DirWalker/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify()],
+  plugins: [vue(), vuetify(), sitemap({ hostname: hostname, dynamicRoutes: [route] })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/DirWalker/"
+  base: route
 })
