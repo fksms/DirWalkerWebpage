a919503685b371559856ef5ddecc7e092deec61c
Shogo Fukushima <59794072+fksms@users.noreply.github.com>
2024-08-08 00:34:20 +0900

    Modified to support unique domain

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,14 @@ import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 import sitemap from "vite-plugin-sitemap";
 
-const hostname = "https://fksms.github.io/";
-const route = "/DirWalker/";
+const hostname = "https://dirwalker.com/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify(), sitemap({ hostname: hostname, dynamicRoutes: [route] })],
+  plugins: [vue(), vuetify(), sitemap({ hostname: hostname })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: route
+  }
 })
